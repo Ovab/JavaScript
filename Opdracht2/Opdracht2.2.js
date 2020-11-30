809e204811bf46afc07d98c00fcd6068b752e8b6
Bavo <61050421+Ovab@users.noreply.github.com>
2020-11-30 08:43:14 +0100

    Opdr 2.2 done

--- Opdracht2/Opdracht2.2.js
+++ Opdracht2/Opdracht2.2.js
@@ -1,4 +1,17 @@
 document.getElementById("bl1").style.backgroundColor = "aqua";
-document.getElementById("bl2").style.backgroundColor = "green"; document.getElementById("bl2").style.offsetHeight="500px"
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("bl1").style.paddingTop = "10px";
+document.getElementById("bl1").style.fontFamily = "Saira Extra Condensed";
+
+document.getElementById("bl2").style.marginTop = "20px";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("bl2").style.fontFamily = "Saira Extra Condensed";
+
+document.getElementById("bl3").style.marginTop = "60px";
 document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.textAlign = "end";
+document.getElementById("bl1").style.fontFamily = "Saira Extra Condensed";
+
 document.getElementById("bl4").style.backgroundColor = "red";
+document.getElementById("bl1").style.fontFamily = "Saira Extra Condensed";
