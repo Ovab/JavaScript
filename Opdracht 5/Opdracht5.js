766ceb56f3e73d77aa1b86e0870e31f6b38016dd
Ovab <bavo.famknol@gmail.com>
2021-03-04 21:08:48 +0100

    Revert "made somecoo stuff? dunno rly"
    This reverts commit 69d53241479568d8923686d8249ac5c4b538f60d.

--- Opdracht 5/Opdracht5.js
+++ Opdracht 5/Opdracht5.js
@@ -1,4 +1,4 @@
-var display1=false
+var display=false
 var display2=false
 var display3=false
 var display4=false
@@ -7,28 +7,165 @@ var display6=false
 var display7=false
 var display8=false
 var display9=false
-function changeImage1(clicked_object) {
-    console.log(clicked_object);
-    console.log(clicked_object.id);
-    console.log("display"+clicked_object.id)
-
-    var a="display"+clicked_object.id
-    if (eval (a)==false) {
-        console.log("BOOOM");
-        document.getElementById(clicked_object.id).src = "cat"+clicked_object.id+".png";
+function changeImage1() {
+    if (display==false) {
+        document.getElementById('cat1').src = "cat1.png";
         display = true
     }
     else {document.getElementById('cat1').src = "cat1.jpg";display=false}
 }
-
+function changeImage2() {
+    if (display2==false) {
+        document.getElementById('cat2').src = "cat2.png";
+        display2 = true
+    }
+    else {document.getElementById('cat2').src = "cat2.jpg";display2=false}
+}
+function changeImage3() {
+    if (display3==false) {
+        document.getElementById('cat3').src = "cat3.png";
+        display3 = true
+    }
+    else {document.getElementById('cat3').src = "cat3.jpg";display3=false}
+}
+function changeImage4() {
+    if (display4==false) {
+        document.getElementById('cat4').src = "cat4.png";
+        display4 = true
+    }
+    else {document.getElementById('cat4').src = "cat4.jpg";display4=false}
+}
+function changeImage5() {
+    if (display5==false) {
+        document.getElementById('cat5').src = "cat5.png";
+        display5 = true
+    }
+    else {document.getElementById('cat5').src = "cat5.jpg";display5=false}
+}
+function changeImage6() {
+    if (display6==false) {
+        document.getElementById('cat6').src = "cat6.png";
+        display6 = true
+    }
+    else {document.getElementById('cat6').src = "cat6.jpg";display6=false}
+}
+function changeImage7() {
+    if (display7==false) {
+        document.getElementById('cat7').src = "cat7.png";
+        display7 = true
+    }
+    else {document.getElementById('cat7').src = "cat7.jpg";display7=false}
+}
+function changeImage8() {
+    if (display8==false) {
+        document.getElementById('cat8').src = "cat8.png";
+        display8 = true
+    }
+    else {document.getElementById('cat8').src = "cat8.jpg";display8=false}
+}
+function changeImage9() {
+    if (display9==false) {
+        document.getElementById('cat9').src = "cat9.png";
+        display9 = true
+    }
+    else {document.getElementById('cat9').src = "cat9.jpg";display9=false}
+}
 function checklike() {
-    var display1=true
-    document.getElementById('2').src = "cat2.jpg";display2=false
-    document.getElementById('3').src = "cat3.jpg";display3=false
-    document.getElementById('4').src = "cat4.jpg";display4=false
-    document.getElementById('5').src = "cat5.jpg";display5=false
-    document.getElementById('6').src = "cat6.jpg";display6=false
-    document.getElementById('7').src = "cat7.jpg";display7=false
-    document.getElementById('8').src = "cat8.jpg";display8=false
-    document.getElementById('9').src = "cat9.jpg";display9=false
+    var display=true
+    document.getElementById('cat2').src = "cat2.jpg";display2=false
+    document.getElementById('cat3').src = "cat3.jpg";display3=false
+    document.getElementById('cat4').src = "cat4.jpg";display4=false
+    document.getElementById('cat5').src = "cat5.jpg";display5=false
+    document.getElementById('cat6').src = "cat6.jpg";display6=false
+    document.getElementById('cat7').src = "cat7.jpg";display7=false
+    document.getElementById('cat8').src = "cat8.jpg";display8=false
+    document.getElementById('cat9').src = "cat9.jpg";display9=false
+}
+function checklike2() {
+    document.getElementById('cat1').src = "cat1.jpg";display=false
+    var display2=true
+    document.getElementById('cat3').src = "cat3.jpg";display3=false
+    document.getElementById('cat4').src = "cat4.jpg";display4=false
+    document.getElementById('cat5').src = "cat5.jpg";display5=false
+    document.getElementById('cat6').src = "cat6.jpg";display6=false
+    document.getElementById('cat7').src = "cat7.jpg";display7=false
+    document.getElementById('cat8').src = "cat8.jpg";display8=false
+    document.getElementById('cat9').src = "cat9.jpg";display9=false
+}
+function checklike3() {
+    document.getElementById('cat1').src = "cat1.jpg";display=false
+    document.getElementById('cat2').src = "cat2.jpg";display2=false
+    display3=true
+    document.getElementById('cat4').src = "cat4.jpg";display4=false
+    document.getElementById('cat5').src = "cat5.jpg";display5=false
+    document.getElementById('cat6').src = "cat6.jpg";display6=false
+    document.getElementById('cat7').src = "cat7.jpg";display7=false
+    document.getElementById('cat8').src = "cat8.jpg";display8=false
+    document.getElementById('cat9').src = "cat9.jpg";display9=false
+}
+function checklike4() {
+    document.getElementById('cat1').src = "cat1.jpg";display=false
+    document.getElementById('cat2').src = "cat2.jpg";display2=false
+    document.getElementById('cat3').src = "cat3.jpg";display3=false
+    display4=true
+    document.getElementById('cat5').src = "cat5.jpg";display5=false
+    document.getElementById('cat6').src = "cat6.jpg";display6=false
+    document.getElementById('cat7').src = "cat7.jpg";display7=false
+    document.getElementById('cat8').src = "cat8.jpg";display8=false
+    document.getElementById('cat9').src = "cat9.jpg";display9=false
+}
+function checklike5() {
+    document.getElementById('cat1').src = "cat1.jpg";display=false
+    document.getElementById('cat2').src = "cat2.jpg";display2=false
+    document.getElementById('cat3').src = "cat3.jpg";display3=false
+    document.getElementById('cat4').src = "cat4.jpg";display4=false
+    display5=true
+    document.getElementById('cat6').src = "cat6.jpg";display6=false
+    document.getElementById('cat7').src = "cat7.jpg";display7=false
+    document.getElementById('cat8').src = "cat8.jpg";display8=false
+    document.getElementById('cat9').src = "cat9.jpg";display9=false
+}
+function checklike6() {
+    document.getElementById('cat1').src = "cat1.jpg";display=false
+    document.getElementById('cat2').src = "cat2.jpg";display2=false
+    document.getElementById('cat3').src = "cat3.jpg";display3=false
+    document.getElementById('cat4').src = "cat4.jpg";display4=false
+    document.getElementById('cat5').src = "cat5.jpg";display5=false
+    display6=true
+    document.getElementById('cat7').src = "cat7.jpg";display7=false
+    document.getElementById('cat8').src = "cat8.jpg";display8=false
+    document.getElementById('cat9').src = "cat9.jpg";display9=false
+}
+function checklike7() {
+    document.getElementById('cat1').src = "cat1.jpg";display=false
+    document.getElementById('cat2').src = "cat2.jpg";display2=false
+    document.getElementById('cat3').src = "cat3.jpg";display3=false
+    document.getElementById('cat4').src = "cat4.jpg";display4=false
+    document.getElementById('cat5').src = "cat5.jpg";display5=false
+    document.getElementById('cat6').src = "cat6.jpg";display6=false
+    display7=true
+    document.getElementById('cat8').src = "cat8.jpg";display8=false
+    document.getElementById('cat9').src = "cat9.jpg";display9=false
+}
+function checklike8() {
+    document.getElementById('cat1').src = "cat1.jpg";display=false
+    document.getElementById('cat2').src = "cat2.jpg";display2=false
+    document.getElementById('cat3').src = "cat3.jpg";display3=false
+    document.getElementById('cat4').src = "cat4.jpg";display4=false
+    document.getElementById('cat5').src = "cat5.jpg";display5=false
+    document.getElementById('cat6').src = "cat6.jpg";display6=false
+    document.getElementById('cat7').src = "cat7.jpg";display7=false
+    display8=true
+    document.getElementById('cat9').src = "cat9.jpg";display9=false
+}
+function checklike9() {
+    document.getElementById('cat1').src = "cat1.jpg";display=false
+    document.getElementById('cat2').src = "cat2.jpg";display2=false
+    document.getElementById('cat3').src = "cat3.jpg";display3=false
+    document.getElementById('cat4').src = "cat4.jpg";display4=false
+    document.getElementById('cat5').src = "cat5.jpg";display5=false
+    document.getElementById('cat6').src = "cat6.jpg";display6=false
+    document.getElementById('cat7').src = "cat7.jpg";display7=false
+    document.getElementById('cat8').src = "cat8.jpg";display8=false
+    display9=true
 }
